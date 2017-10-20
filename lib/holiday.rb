7690de1853dcaed43b6aa4f92c66922992b230dc
Pamela Frieze <pfrieze@gmail.com>
2017-10-20 19:46:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,5 +73,5 @@ def all_holidays_with_bbq(holiday_hash)
     holidays_hash.map do |holidays_hash, supplies|
     holidays_hash if supplies.include?("BBQ")
     end
-  end.flatten.compact
+  end.flatten.compact  #compact removes all nil elements
 end
